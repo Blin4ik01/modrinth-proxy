d3edac85bb3c033ad2aa144daf7e10e0b8fc382a
b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0 <blinkpik123@gmail.com>
2025-10-21 15:57:35 +0300

    - Я здесь

--- app/components/TopNav.js
+++ app/components/TopNav.js
@@ -10,11 +10,12 @@ export default function TopNav() {
           <a href="https://client.white-minecraft.net" className="top-nav-link" data-tooltip="Скачать Minecraft клиент и сервер всех версий">
             Клиент<span className="top-nav-separator">/</span>Сервер
           </a>
-          <a href="https://ping.white-minecraft.net" className="top-nav-link" data-tooltip="Проверить доступность Minecraft серверов">
-            Пинг Серверов
-          </a>
+         
           <a href="https://dm.white-minecraft.net" className="top-nav-link" data-tooltip="Создать меню для DeluxeMenu плагина">
             Редактор DeluxeMenu
+          </a>
+          <a href="https://ping.white-minecraft.net" className="top-nav-link" data-tooltip="Проверить доступность Minecraft серверов">
+            Пинг Серверов
           </a>
         </div>
       </div>
