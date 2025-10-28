198d8c81a029dbaf299b8b9d0528e570f4b19abb
b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0 <blinkpik123@gmail.com>
2025-10-28 18:50:51 +0300

    - fix?

--- next.config.js
+++ next.config.js
@@ -6,34 +6,10 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   fallbacks: {
     document: '/offline.html',
   },
-  cacheOnFrontEndNav: true,
+  cacheOnFrontEndNav: false,
   workboxOptions: {
     disableDevLogs: true,
-    runtimeCaching: [
-      {
-        urlPattern: /^\/offline\.html$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'offline-page',
-          expiration: {
-            maxEntries: 1,
-            maxAgeSeconds: 365 * 24 * 60 * 60,
-          },
-        },
-      },
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'pages-cache',
-          expiration: {
-            maxEntries: 200,
-            maxAgeSeconds: 24 * 60 * 60,
-          },
-          networkTimeoutSeconds: 10,
-        },
-      },
-    ],
+    cleanupOutdatedCaches: true,
   },
 })
 
