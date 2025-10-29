e592541d2c4a81dcd9345e80652dff1a482a8eec
b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0 <blinkpik123@gmail.com>
2025-10-29 10:32:19 +0300

    - топ100

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,4 +1,6 @@
-export default function sitemap() {
+import { searchMods } from '@/lib/modrinth'
+
+export default async function sitemap() {
   const baseUrl = 'https://modrinth.black'
   
   const routes = [
@@ -21,6 +23,38 @@ export default function sitemap() {
     priority: route === '' ? 1.0 : 0.8,
   }))
 
-  return staticPages
+  const resourceTypes = [
+    { facet: 'mod', route: 'mod' },
+    { facet: 'plugin', route: 'plugin' },
+    { facet: 'shader', route: 'shader' },
+    { facet: 'resourcepack', route: 'resourcepack' },
+    { facet: 'datapack', route: 'datapack' },
+    { facet: 'modpack', route: 'modpack' },
+  ]
+
+  const dynamicPages = []
+
+  for (const type of resourceTypes) {
+    try {
+      const data = await searchMods({
+        facets: [['project_type:' + type.facet]],
+        limit: 100,
+        index: 'downloads',
+      })
+
+      const projects = data.hits.map((project) => ({
+        url: `${baseUrl}/${type.route}/${project.slug}`,
+        lastModified: new Date(project.date_modified || project.date_created),
+        changeFrequency: 'weekly',
+        priority: 0.6,
+      }))
+
+      dynamicPages.push(...projects)
+    } catch (error) {
+      console.error(`Error fetching ${type.facet}:`, error)
+    }
+  }
+
+  return [...staticPages, ...dynamicPages]
 }
 
