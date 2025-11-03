b805f69de6ca9ec3937899df01792e27ebc6e0d4
b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0 <blinkpik123@gmail.com>
2025-11-04 00:07:38 +0300

    - добавление новых инструментов - Изменение ссылок у действующих инструментов

--- app/components/TopNav.js
+++ app/components/TopNav.js
@@ -7,11 +7,16 @@ export default function TopNav() {
             <span className="top-nav-dot"></span>
             ModrinthProxy
           </span>
-          <a href="https://client.white-minecraft.net" className="top-nav-link" data-tooltip="Скачать Minecraft клиент и сервер всех версий">
+          <a href="https://client.modrinth.black" className="top-nav-link" data-tooltip="Скачать Minecraft клиент и сервер всех версий">
             Клиент<span className="top-nav-separator">/</span>Сервер
           </a>
-         
-          <a href="https://dm.white-minecraft.net" className="top-nav-link" data-tooltip="Создать меню для DeluxeMenu плагина">
+          <a href="https://rp.modrinth.black" className="top-nav-link" data-tooltip="Перепаковка защищённых ресурспаков">
+            RPПерепак
+          </a>
+          <a href="https://jar.modrinth.black" className="top-nav-link" data-tooltip="Перевод плагинов онлайн (InJarTranslator)">
+            Перевод Плагинов
+          </a>
+          <a href="https://dm.modrinth.black" className="top-nav-link" data-tooltip="Редактор DeluxeMenu и AbstractMenus">
             Редактор DeluxeMenu
           </a>
           <a href="https://ping.white-minecraft.net" className="top-nav-link" data-tooltip="Проверить доступность Minecraft серверов">
