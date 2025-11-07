e5c16f5a31d67990ec0109edebba6ed885799640
b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0 <blinkpik123@gmail.com>
2025-11-07 15:13:08 +0300

    - метрика яндекса

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Nunito } from "next/font/google"
+import Script from 'next/script'
 import { Suspense } from 'react'
 import MobileNav from './components/MobileNav'
 import Navigation from './components/Navigation'
@@ -48,8 +49,18 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105182235', 'ym');
+          ym(105182235, 'init', {ssr:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`}
+        </Script>
       </head>
       <body className={`${nunito.className} overflow-x-hidden min-h-screen m-0`}>
+        <noscript dangerouslySetInnerHTML={{ __html: '<div><img src="https://mc.yandex.ru/watch/105182235" style="position:absolute; left:-9999px;" alt="" /></div>' }} />
         <VersionsPreloader />
         <TopNav />
         <nav className="bg-modrinth-darker shadow-lg hidden lg:block">
